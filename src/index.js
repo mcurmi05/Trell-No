731602f7af19fd365ca236f37a11f182c552f9be
mcurmi05 <mcurmi05@gmail.com>
2025-07-01 14:40:48 +1000

    Dropdown functionality in the sidebar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+//Index JS contains javascript that is fundamental to the website:
+  //Sidebar functionality
+
+
 import './index.css'
 
 const navbar = document.querySelector('#navbar')
 const sidebar = document.querySelector('#sidebar');
 const sidebarToggleBtn = document.querySelector('#sidebar-toggle-button');
 
-//Our button toggle the sidebar on the left
+const sidebarBoardsBtn = document.querySelector('#sidebar-boards');
+const userBoardsDiv = document.querySelector('#boards')
+const boardsArrow = document.querySelector('.sidebar-arrow');
+
+
+//Burger button in the navbar to toggle the side bar
 sidebarToggleBtn.addEventListener('click', () => {
-  sidebarToggleBtn.classList.toggle('active');
+  sidebar.classList.toggle('active');
 });
+
+//Clicking the boards div in the sidebar should rotate the arrow and collapse/uncollapse the boards under it
+sidebarBoardsBtn.addEventListener('click', () => {
+  userBoardsDiv.classList.toggle('collapsed');
+  boardsArrow.classList.toggle('rotated');
+});
+
+
